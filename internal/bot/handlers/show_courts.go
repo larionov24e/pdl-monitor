2c66796ad15135799a2d24f4e8a04f47eed2cad0
larionove <yehor.larionov@mpfunds.com>
2026-05-02 14:12:04 +0300

    add deploy.yml

--- internal/bot/handlers/show_courts.go
+++ internal/bot/handlers/show_courts.go
@@ -84,7 +84,7 @@ func (t *SelectTime) Handle(_ context.Context, update tgbotapi.Update, s *sessio
 
 	rows = append(rows, tgbotapi.NewInlineKeyboardRow(
 		tgbotapi.NewInlineKeyboardButtonData(
-			"Start Monitor",
+			"Start monitor",
 			"start_monitor:",
 		),
 	))
